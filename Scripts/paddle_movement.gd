0269caf24ca39448e8c645bd40a37153fb7e7a93
Carmine D'Amico <github@cdamico.de>
2025-07-07 07:40:15 +0200

    little code improvement

--- Scripts/paddle_movement.gd
+++ Scripts/paddle_movement.gd
@@ -9,13 +9,7 @@ func _process(delta: float) -> void:
 	var upPressed = Input.is_action_pressed("move_up_p1" if player == Players.P1 else "move_up_p2")
 	var downPressed = Input.is_action_pressed("move_down_p1" if player == Players.P1 else "move_down_p2")
 	
-	var direction = 0;
-	if upPressed:
-		direction = -1
-	elif downPressed:
-		direction = 1
-	else:
-		direction = 0
+	var direction = -1 if upPressed else 1 if downPressed else 0;
 	
 	var movement = direction * playerVelocity * delta
 	var newPosition = clamp(position.y + movement, 0, get_viewport_rect().size.y)
